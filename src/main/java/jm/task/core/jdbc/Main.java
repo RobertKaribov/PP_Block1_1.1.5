ca9a06c26d284b750feaf917255d4392d3c6bb34
MrRobert09 <jobbox099@mail.ru>
2024-03-19 01:27:53 +0300

    1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,39 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        // Создание таблицы User(ов)
+        userService.createUsersTable();
+
+        // Добавление 4 User(ов) в таблицу
+        userService.saveUser("John", "Doe", (byte) 30);
+        userService.saveUser("Jane", "Smith", (byte) 25);
+        userService.saveUser("Michael", "Johnson", (byte) 35);
+        userService.saveUser("Emily", "Brown", (byte) 28);
+
+        // Вывод всех User из базы и вывод в консоль
+        List<User> users = userService.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        // Очистка таблицы User(ов)
+        userService.cleanUsersTable();
+
+        // Удаление таблицы
+        userService.dropUsersTable();
     }
 }
+
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/mydbtest";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "root";
+
+    public static Connection connect() throws SQLException {
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+    }
 }
